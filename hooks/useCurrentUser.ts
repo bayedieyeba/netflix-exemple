ccea3edf9931ca3fac0dfa1b2b867f6763fdf805
bayedieyeba <baye.dieye@aeservices.digital>
2023-03-19 03:45:45 +0000

    final project

---
+++ hooks/useCurrentUser.ts
@@ -0,0 +1,17 @@
+// npm install swr
+
+import useSwr from 'swr'
+
+import fetcher from '@/libs/fetcher';
+
+const useCurrentUser = () => {
+  const { data, error, isLoading, mutate } = useSwr('/api/current', fetcher);
+  return {
+    data,
+    error,
+    isLoading,
+    mutate,
+  }
+};
+
+export default useCurrentUser;
